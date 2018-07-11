ae50719ed9dcbb4d6685c4a1fd5d13b66a9515f6
Pierre VIGIER <pierre.vigier@gmail.com>
2018-07-11 11:40:05 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Net::StatsD::Client::Telegraf - Blah blah blah
+
+# SYNOPSIS
+
+    use Net::StatsD::Client::Telegraf;
+
+# DESCRIPTION
+
+Net::StatsD::Client::Telegraf is
+
+# AUTHOR
+
+Pierre VIGIER <pierre.vigier@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2018- Pierre VIGIER
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Net/StatsD/Client/Telegraf.pm
@@ -0,0 +1,39 @@
+package Net::StatsD::Client::Telegraf;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Net::StatsD::Client::Telegraf - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Net::StatsD::Client::Telegraf;
+
+=head1 DESCRIPTION
+
+Net::StatsD::Client::Telegraf is
+
+=head1 AUTHOR
+
+Pierre VIGIER E<lt>pierre.vigier@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2018- Pierre VIGIER
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
